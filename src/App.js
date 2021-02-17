bcb0046375f4dcb277c6bae9293908b9ec9f2c53
muhammad wasi81 <wasiarain81@gmail.com>
2021-02-17 23:20:38 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello world from aamir pinger</h1>
+        <p>lorem</p>
+    </div>
+  );
+}
+
+export default App;
